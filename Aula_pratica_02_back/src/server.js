31127ca135655c0681f7b39aac3dd12e81ff41d2
leticiapuga <leticiademirandapuga@gmail.com>
2024-08-01 12:51:26 -0300

    ...

--- Aula_pratica_02_back/src/server.js
+++ Aula_pratica_02_back/src/server.js
@@ -25,53 +25,56 @@ app.post("/users", async (request, response) => {
         }
     })
 
-    response.status(201).json(user);
+    response.status(201).json(user); 
 });
 
 
-app.put("/usuario/:id", async (request, response) => {
+app.put(("/users/:id"), async (request, response) => {
     
-    const { nome, email, data_nascimento } = request.body;
+    const { name, email } = request.body;
     
     const { id } = request.params;
 
-    // EXECUTA UMA CONSULTA SQL PARA SELECIONAR UM PACIENTE PELO ID
-    const pacientes = await pool.query('SELECT * FROM pacientes WHERE id = $1', [id]);
+ 
+    const user = await prisma.user.findFirst({
+        where: { id }
+    })
 
-    // SE O PACIENTE EXISTIR, EXECUTA UMA CONSULTA PARA ATUALIZAR SEUS DADOS
-    if (pacientes) {
-        const pacientes = await pool.query(
-            'UPDATE pacientes SET nome = $1, email = $2, data_nascimento = $3 WHERE id = $4',
-            [nome, email, data_nascimento, id]
-        );
-        // RETORNA UMA RESPOSTA VAZIA COM STATUS 200 (OK)
+    
+    if (user) {
+        const user = await prisma.user.update({
+            data:{ name, email },
+            where: { id }
+        })
+       
+        response.status(200).json(user);
+
+    } else {
+        response.status(404).json("User not found"); 
+    }
+})
+
+
+app.delete("/users/:id", async (request, response) => {
+
+    const { id } = request.params;
+    
+    const user = await prisma.user.findFirst({
+        where: { id }
+    })
+
+
+    if (user) {
+        await prisma.user.delete
+        
         response.status(200).json('');
     } else {
-        // SE O PACIENTE NÃO FOR ENCONTRADO, RETORNA UM ERRO 404 (NOT FOUND)
+       
         response.status(404).json("Usuário não encontrado");
     }
 });
 
-// DEFINE UMA ROTA (DELETAR) DELETE PARA O ENDPOINT '/usuario/:id'
-app.delete("/usuario/:id", async (request, response) => {
-    // PEGA O 'id' DOS PARÂMETROS DA ROTA
-    const { id } = request.params;
 
-    // EXECUTA UMA CONSULTA SQL PARA SELECIONAR UM PACIENTE PELO ID
-    const pacientes = await pool.query('SELECT * FROM pacientes WHERE id = $1', [id]);
-
-    // SE O PACIENTE EXISTIR, EXECUTA UMA CONSULTA PARA DELETAR O REGISTRO
-    if (pacientes.rows.length > 0) {
-        await pool.query('DELETE FROM pacientes WHERE id = $1', [id]);
-        // RETORNA UMA RESPOSTA VAZIA COM STATUS 200 (OK)
-        response.status(200).json('');
-    } else {
-        // SE O PACIENTE NÃO FOR ENCONTRADO, RETORNA UM ERRO 404 (NOT FOUND)
-        response.status(404).json("Usuário não encontrado");
-    }
-});
-
-// INICIA O SERVIDOR NA PORTA 3000 E IMPRIME UMA MENSAGEM NO TERMINAL
 app.listen(3000, () => {
     console.log("Running server");
 });
